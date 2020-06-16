28469cf9d26d616211616789604bf8635e2b4c38
ShaunFerns <shaun.ferns@tudublin.ie>
2020-06-16 13:06:09 +0100

    intial commit

---
+++ Assets/_Scripts/DelayedSoundManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class DelayedSoundManager : MonoBehaviour
+{
+    public AudioSource audioSourcePacmandMusic;
+    public AudioSource audioSourceDies;
+
+    public void ACTION_PlayMusicNow()
+    {
+        audioSourcePacmandMusic.Play();
+    }
+
+    public void ACTION_PlayDiesSoundAfterDelay()
+    {
+        float delay = 1.0F;
+        audioSourceDies.PlayDelayed(delay);
+    }
+}
